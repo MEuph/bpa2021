33ba5ac8027a484907c7948f59ff8445ed8b3b10
MEuph <christophermharris9999@gmail.com>
2020-09-29 09:57:44 -0500

    Initial Commit
    Sep 29, 2020

---
+++ BPA 2021 Project/README.txt
@@ -0,0 +1,2 @@
+bpa2021 is the gradle project which eclipse-workspace references
+Any changes made to bpa2021 will be reflected in eclipse
---
+++ BPA 2021 Project/bpa2021/desktop/src/com/cognitivethought/bpa/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cognitivethought.bpa.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cognitivethought.bpa.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
