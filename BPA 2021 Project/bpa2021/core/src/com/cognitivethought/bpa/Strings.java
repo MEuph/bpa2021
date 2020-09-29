6ea235a12c9ceb4f689a5cfa870568b4bd0b60c5
MEuph <christophermharris9999@gmail.com>
2020-09-29 10:33:31 -0500

    Added example support for Backendless for later reference

---
+++ BPA 2021 Project/bpa2021/core/src/com/cognitivethought/bpa/Strings.java
@@ -0,0 +1,8 @@
+package com.cognitivethought.bpa;
+
+public class Strings {
+	
+	public static final String APP_ID = "56E69610-EBDD-52D4-FFF7-EB3E7A675500";
+	public static final String SECRET_KEY = "6FADE322-B1A0-430D-9EE5-5EE9152EA4C3";
+	
+}
--- BPA 2021 Project/bpa2021/core/src/com/cognitivethought/bpa/game/Game.java
+++ BPA 2021 Project/bpa2021/core/src/com/cognitivethought/bpa/game/Game.java
@@ -1,17 +1,44 @@
 package com.cognitivethought.bpa.game;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.backendless.Backendless;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.cognitivethought.bpa.Strings;
 
 public class Game extends ApplicationAdapter {
+	
 	SpriteBatch batch;
 	Texture img;
 	
+	@SuppressWarnings("rawtypes")
 	@Override
 	public void create () {
+		Backendless.initApp(Strings.APP_ID, Strings.SECRET_KEY);
+		
+		HashMap<String, String> test = new HashMap<>();
+		test.put("first", "second");
+		Backendless.Data.of("Test").save(test, new AsyncCallback<Map>() {
+
+			@Override
+			public void handleResponse(Map response) {
+				System.out.println("Item saved");
+			}
+
+			@Override
+			public void handleFault(BackendlessFault fault) {
+				System.err.println("<ERROR>: Server reported an error: " + fault.getMessage());
+			}
+			
+		});
+		
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
