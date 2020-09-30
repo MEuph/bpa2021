2fa9cdd588d1795d420daef59d89e51e749c10d0
MEuph <54487141+MEuph@users.noreply.github.com>
2020-09-30 18:39:15 -0500

    Began implementing launcher title

--- BPA 2021 Project/bpa2021/core/src/com/cognitivethought/bpa/Strings.java
+++ BPA 2021 Project/bpa2021/core/src/com/cognitivethought/bpa/Strings.java
@@ -5,11 +5,13 @@ public class Strings {
 	public static final String APP_ID = "56E69610-EBDD-52D4-FFF7-EB3E7A675500";
 	public static final String SECRET_KEY = "6FADE322-B1A0-430D-9EE5-5EE9152EA4C3";
 	
-	public static final String LNUI_EMAIL = " [Email]";
-	public static final String LNUI_PASSWORD = " [Password]";
+	public static final String LNUI_EMAIL = "[Email]";
+	public static final String LNUI_PASSWORD = "[Password]";
 	public static final String LNUI_CREATE_ACCOUNT = "Create Account";
-	public static final String LNUI_USERNAME = " [Username]";
+	public static final String LNUI_USERNAME = "[Username]";
 	public static final String LNUI_LOGIN = "Login";
+	public static final String LNUI_TITLE = "[GAME]";
 	
 	public static final String ERROR_BE3033 = "Sorry, an account with that email has already been created!";
+	public static final String ERROR_BE3003 = "Invalid username or password";
 }
--- BPA 2021 Project/bpa2021/core/src/com/cognitivethought/bpa/launcher/Launcher.java
+++ BPA 2021 Project/bpa2021/core/src/com/cognitivethought/bpa/launcher/Launcher.java
@@ -16,6 +16,8 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
@@ -27,89 +29,101 @@ import com.badlogic.gdx.utils.Align;
 import com.cognitivethought.bpa.Strings;
 
 public class Launcher extends ApplicationAdapter {
-	
+
 	Stage newAccountStage;
 	Stage loginStage;
 	Stage currentStage;
-	
+
 	BackendlessUser currentUser = null;
-	
+
 	VerticalGroup na_elements;
-	
+
 	TextField na_username, na_email, na_password;
 	TextButton na_createUser;
-	
+
 	VerticalGroup login_elements;
-	
+
 	TextField login_username, login_password;
 	TextButton login_submit;
 	
-	BitmapFont font;
+	Label title;
 	
+	BitmapFont font;
+
 	@Override
-	public void create () {
+	public void create() {
 		Backendless.initApp(Strings.APP_ID, Strings.SECRET_KEY);
-		
+
+		font = new BitmapFont();
+
 		na_elements = new VerticalGroup();
 		login_elements = new VerticalGroup();
-		
+
 		na_elements.align(Align.center);
 		login_elements.align(Align.center);
-		
+
 		na_elements.space(20f);
 		login_elements.space(20f);
-		
+
 		na_elements.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
 		login_elements.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
 		
-		font = new BitmapFont();
-		
 		newAccountStage = new Stage();
 		loginStage = new Stage();
-		
+
 		setStage(loginStage);
-		
+
 		populateUI();
 	}
-	
+
 	public void setStage(Stage s) {
-		if (currentStage != null) currentStage.unfocusAll();
+		if (currentStage != null)
+			currentStage.unfocusAll();
 		currentStage = s;
 		Gdx.input.setInputProcessor(s);
 	}
-	
+
 	public void login(String email, String password) {
 		Backendless.UserService.login(email, password, new AsyncCallback<BackendlessUser>() {
 			@Override
 			public void handleFault(BackendlessFault fault) {
-				System.err.println(fault.getCode());
+				switch (fault.getCode()) {
+				case "3003":
+					System.err.println(Strings.ERROR_BE3003);
+					break;
+				default:
+					System.err.println("Error logging in! " + fault.getMessage());
+					break;
+				}
 			}
-			
+
 			@Override
 			public void handleResponse(BackendlessUser response) {
 				System.out.println("Successfully logged in as " + response.getProperty("name"));
 			}
 		});
 	}
-	
+
 	public void createUser(String username, String email, String password) {
 		BackendlessUser createUser = new BackendlessUser();
-		
+
 		createUser.setEmail(email);
 		createUser.setPassword(password);
 		createUser.setProperty("name", username);
-		
+
 		Backendless.UserService.register(createUser, new AsyncCallback<BackendlessUser>() {
 			@Override
 			public void handleFault(BackendlessFault fault) {
 				switch (fault.getCode()) {
 				case "3033":
 					System.err.println(Strings.ERROR_BE3033);
+					break;
 				default:
 					System.err.println(fault.getCode());
+					break;
 				}
 			}
-			
+
 			@Override
 			public void handleResponse(BackendlessUser response) {
 				System.out.println("Successfully created new user, " + response.getProperty("name"));
@@ -117,8 +131,9 @@ public class Launcher extends ApplicationAdapter {
 			}
 		});
 	}
-	
-	private boolean isClickingOnUIElement(float mouseX, float mouseY, Actor ... actors) {
+
+	@SuppressWarnings("unused")
+	private boolean isClickingOnUIElement(float mouseX, float mouseY, Actor... actors) {
 		for (Actor a : actors) {
 			if (new Rectangle(a.getX(), a.getY(), a.getWidth(), a.getHeight()).contains(mouseX, mouseY)) {
 				return true;
@@ -128,25 +143,49 @@ public class Launcher extends ApplicationAdapter {
 		}
 		return false;
 	}
-	
+
+	private boolean isClickingOnUIElement(Actor relativeTo, float mouseX, float mouseY, Actor... actors) {
+		for (Actor a : actors) {
+			if (new Rectangle(a.getX() + relativeTo.getX(), a.getY() + relativeTo.getY(), a.getWidth(), a.getHeight())
+					.contains(mouseX, mouseY)) {
+				return true;
+			} else {
+				continue;
+			}
+		}
+		return false;
+	}
+
 	private void populateUI() {
 		// -- GENERAL -- //
 		TextFieldStyle textStyle = new TextFieldStyle();
 		textStyle.font = font;
-		Pixmap bgColor = new Pixmap(200, (int)textStyle.font.getLineHeight(), Pixmap.Format.RGB888);
+		Pixmap bgColor = new Pixmap(200, (int) textStyle.font.getLineHeight(), Pixmap.Format.RGB888);
 		bgColor.setColor(Color.WHITE);
 		bgColor.fill();
 		textStyle.fontColor = Color.GRAY;
 		textStyle.background = new Image(new Texture(bgColor)).getDrawable();
+		textStyle.background.setLeftWidth(textStyle.background.getLeftWidth() + 5);
 		
 		TextButtonStyle buttonStyle = new TextButtonStyle();
 		bgColor.setColor(Color.DARK_GRAY);
 		bgColor.fill();
+		buttonStyle.down = new Image(new Texture(bgColor)).getDrawable();
+		buttonStyle.up = new Image(new Texture(bgColor)).getDrawable();
 		buttonStyle.font = font;
 		buttonStyle.fontColor = Color.WHITE;
 		buttonStyle.overFontColor = Color.RED;
 		buttonStyle.downFontColor = Color.GRAY;
 		
+		LabelStyle titleStyle = new LabelStyle();
+		titleStyle.font = font;
+		titleStyle.fontColor = Color.WHITE;
+		bgColor.setColor(Color.WHITE);
+		titleStyle.background = new Image(new Texture(bgColor)).getDrawable();
+		title = new Label(Strings.LNUI_TITLE, titleStyle);
+		title.setSize(200, 20);
+		title.setPosition(100, 200);
+
 		// -- NEW ACCOUNT UI -- //
 		na_email = new TextField(Strings.LNUI_EMAIL, new TextFieldStyle(textStyle));
 		na_password = new TextField(Strings.LNUI_PASSWORD, new TextFieldStyle(textStyle));
@@ -156,6 +195,7 @@ public class Launcher extends ApplicationAdapter {
 		na_createUser.setBackground(textStyle.background);
 		na_createUser.setSize(200, font.getLineHeight() + 10);
 		na_createUser.setPosition(100, 145);
+		na_createUser.align(Align.center);
 		
 		na_createUser.addListener(new ClickListener() {
 			@Override
@@ -163,7 +203,7 @@ public class Launcher extends ApplicationAdapter {
 				createUser(na_username.getText(), na_email.getText(), na_password.getText());
 			}
 		});
-		
+
 		na_email.addListener(new FocusListener() {
 			@Override
 			public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
@@ -181,7 +221,7 @@ public class Launcher extends ApplicationAdapter {
 				}
 			}
 		});
-		
+
 		na_username.addListener(new FocusListener() {
 			@Override
 			public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
@@ -199,7 +239,7 @@ public class Launcher extends ApplicationAdapter {
 				}
 			}
 		});
-		
+
 		na_password.addListener(new FocusListener() {
 			@Override
 			public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
@@ -217,38 +257,43 @@ public class Launcher extends ApplicationAdapter {
 				}
 			}
 		});
-		
+
 		newAccountStage.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				if (!isClickingOnUIElement(x, y, na_email, na_password, na_username, na_createUser)) {
+				if (!isClickingOnUIElement(na_elements, x, y, na_email, na_password, na_username, na_createUser)) {
 					newAccountStage.unfocusAll();
 				}
 				super.clicked(event, x, y);
 			}
 		});
-		
-		na_username.setSize(200, textStyle.font.getLineHeight());
-		na_email.setSize(200, textStyle.font.getLineHeight());
-		na_password.setSize(200, textStyle.font.getLineHeight());
+
+		na_username.setSize(200, textStyle.font.getLineHeight() + 10);
+		na_email.setSize(200, textStyle.font.getLineHeight() + 10);
+		na_password.setSize(200, textStyle.font.getLineHeight() + 10);
 		
 		na_username.setPosition(100, 220);
 		na_email.setPosition(100, 200);
 		na_password.setPosition(100, 180);
 		
+		na_username.setAlignment(Align.center);
+		na_email.setAlignment(Align.center);
+		na_password.setAlignment(Align.center);
+		
 		na_elements.addActor(na_username);
 		na_elements.addActor(na_email);
 		na_elements.addActor(na_password);
 		na_elements.addActor(na_createUser);
-		
+
 		// -- LOGIN UI -- //
 		login_password = new TextField(Strings.LNUI_PASSWORD, new TextFieldStyle(textStyle));
 		login_username = new TextField(Strings.LNUI_USERNAME, new TextFieldStyle(textStyle));
-		
+
 		login_submit = new TextButton(Strings.LNUI_LOGIN, buttonStyle);
 		login_submit.setBackground(textStyle.background);
 		login_submit.setSize(200, font.getLineHeight() + 10);
 		login_submit.setPosition(100, 145);
+		login_submit.align(Align.center);
 		
 		login_submit.addListener(new ClickListener() {
 			@Override
@@ -256,7 +301,7 @@ public class Launcher extends ApplicationAdapter {
 				login(login_username.getText(), login_password.getText());
 			}
 		});
-		
+
 		login_username.addListener(new FocusListener() {
 			@Override
 			public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
@@ -274,7 +319,7 @@ public class Launcher extends ApplicationAdapter {
 				}
 			}
 		});
-		
+
 		login_password.addListener(new FocusListener() {
 			@Override
 			public void keyboardFocusChanged(FocusEvent event, Actor actor, boolean focused) {
@@ -292,22 +337,25 @@ public class Launcher extends ApplicationAdapter {
 				}
 			}
 		});
-		
+
 		loginStage.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				if (!isClickingOnUIElement(x, y, login_password, login_username, login_submit)) {
+				if (!isClickingOnUIElement(login_elements, x, y, login_password, login_username, login_submit)) {
 					loginStage.unfocusAll();
 				}
 				super.clicked(event, x, y);
 			}
 		});
-		
-		login_username.setSize(200, textStyle.font.getLineHeight());
-		login_password.setSize(200, textStyle.font.getLineHeight());
-		
+
+		login_username.setSize(200, textStyle.font.getLineHeight() + 10);
+		login_password.setSize(200, textStyle.font.getLineHeight() + 10);
+
 		login_username.setPosition(100, 220);
 		login_password.setPosition(100, 200);
+
+		login_username.setAlignment(Align.center);
+		login_password.setAlignment(Align.center);
 		
 		login_elements.addActor(login_username);
 		login_elements.addActor(login_password);
@@ -316,22 +364,24 @@ public class Launcher extends ApplicationAdapter {
 		// -- FINALIZING -- //
 		loginStage.addActor(login_elements);
 		newAccountStage.addActor(na_elements);
+		loginStage.addActor(title);
+		newAccountStage.addActor(title);
 	}
-	
+
 	public void update() {
-		
+
 	}
-	
+
 	@Override
-	public void render () {
+	public void render() {
 		update();
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		currentStage.draw();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		newAccountStage.dispose();
 		loginStage.dispose();
 	}
